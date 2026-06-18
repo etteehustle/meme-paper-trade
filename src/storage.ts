a2e7606a0c92fa5eea3f1e3f2fa77b42fabb727a
tee <etteehustle@gmail.com>
2026-06-18 21:57:49 +0700

    fix: default value display to usd

--- src/storage.ts
+++ src/storage.ts
@@ -13,7 +13,7 @@ export const loadPositions = () => read<Position[]>("mpt.positions", []);
 export const loadOrders = () => read<LimitOrder[]>("mpt.orders", []);
 export const loadTrades = () => read<TradeEvent[]>("mpt.trades", []);
 export const loadFees = () => read<FeeConfig>("mpt.fees", { slippagePct: 1, bribeSol: 0.002, txFeeSol: 0.00001 });
-export const loadUsdMode = () => read<boolean>("mpt.usdMode", false);
+export const loadUsdMode = () => read<boolean>("mpt.usdMode", true);
 export const loadLastAddress = () => read<string>("mpt.lastAddress", "");
 export const loadAccount = () => read<AccountState>("mpt.account", { startingCapitalSol: 5, cashSol: 5 });
 
